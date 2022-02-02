912a3e1149ffb66771e0ce33454744d374826b36
planetamusic <98907451+planetamusic@users.noreply.github.com>
2022-02-02 16:56:26 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script>
+function click() {
+if (event.button==2||event.button==3) {
+oncontextmenu='return false';
+  }
+}
+document.onmousedown=click
+document.oncontextmenu = new Function("return false;")
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link rel="stylesheet" type="text/css" href="css/style2.css"/>
+</head>
+<body>  
+<table width="111" height="62" border="0" cellspacing="0" cellpadding="0"><tr><td width="111" background="../../imagem/bg_visitas2.jpg" style="background-repeat: no-repeat; background-position: center;" align="center" valign="middle"><div id="conteudo">
+<span class="visitas">68.846</span></div></div></td></tr></table>
+</body>
+</html>
